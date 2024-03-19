456d66f8eb0a88e3d2a8e840992e637cfe01384b
Julie Zenou <me@juliezen.com>
2024-03-19 17:16:04 +1100

    cleaned up code

--- game.js
+++ game.js
@@ -10,7 +10,6 @@ var level = 0;
 
 var userIndex = 0;
 
-// Listening for when buttons are clicked
 $(".btn").click(function(event) {
     var userChosenColour = event.target.id;
 
@@ -20,12 +19,8 @@ $(".btn").click(function(event) {
     userClickedPattern.push(userChosenColour);
 
     checkAnswer(userIndex);
-    // if user clicked the same as shown
-    //nextSequence();
-    // else game end
 })
 
-// listening for when any button is clicked
 $("body").keypress(function(event){
     if (!gameHasStarted) {
         nextSequence();
@@ -33,9 +28,9 @@ $("body").keypress(function(event){
     gameHasStarted = true;
   });
 
+
 function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
-        console.log("user got it right");
         userIndex++;
 
         if (currentLevel == (gamePattern.length-1)) {
@@ -47,7 +42,6 @@ function checkAnswer(currentLevel) {
         }
         
     } else {
-        console.log("game over");
         gameOver();
     }
     
@@ -83,8 +77,6 @@ function nextSequence() {
     playSound(randomChosenColour);
     
 }
-
-
 
 function animatePress (currentColour) {
     $("#" + currentColour).addClass("pressed");
