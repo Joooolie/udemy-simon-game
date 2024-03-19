5544f059de174ed8a9e75b3e5ccc433c35d38bb5
Julie Zenou <me@juliezen.com>
2024-03-19 16:16:04 +1100

    when clicking on buttons, pattern is stored, music played and button press animation activated

--- game.js
+++ game.js
@@ -2,9 +2,9 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
-nextSequence();
+var userClickedPattern = [];
 
-//playSound(randomChosenColour);
+nextSequence();
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
@@ -12,13 +12,28 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
 
     gamePattern.push(randomChosenColour);
+    animatePress(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(80).fadeIn(80);
+    playSound(randomChosenColour);
     
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
 }
 
-// console.log($("div:button"));
-// $(":button").click(function() {
-//     console.log("button clicked");
-// })
+$(".btn").click(function(event) {
+    var userChosenColour = event.target.id;
+    animatePress(userChosenColour);
+    playSound(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+})
+
+function animatePress (currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function playSound (name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
